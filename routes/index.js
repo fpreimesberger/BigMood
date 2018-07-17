492a2611708c125c0e88e08d04066d19eabaad33
fpriemesberger <fpreimesberger@gmail.com>
2018-07-16 21:13:51 -0700

    started adding playlists and hyperlinks

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Big_Mood' });
 });
 
+router.get('/playlist/happy', function(req, res, next) {
+  res.render('playlist/happy', { mood: 'Happy' });
+});
+router.get('/playlist/angsty', function(req, res, next) {
+  res.render('playlist/angsty', { mood: 'Angsty' });
+});
+router.get('/playlist/sad', function(req, res, next) {
+  res.render('playlist/sad', { mood: 'Sad' });
+});
+
 module.exports = router;
