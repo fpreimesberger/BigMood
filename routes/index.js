3d86d8bbb32bb565f86dbc8b2c86c8dd9e82a0aa
fpriemesberger <fpreimesberger@gmail.com>
2018-07-19 12:54:34 -0700

    fixed up some routes and layout

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// playlist arrays here
+let angsty = '<iframe src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DX3YSRoSdA634" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Big_Mood' });
 });
 
+// playlists and moods
 router.get('/playlist/happy', function(req, res, next) {
-  res.render('playlist/happy', { mood: 'Happy' });
+  res.render('playlist/happy', { mood: 'happy' });
 });
 router.get('/playlist/angsty', function(req, res, next) {
-  res.render('playlist/angsty', { mood: 'Angsty' });
+  res.render('playlist/angsty', { mood: 'angsty', playlist: angsty });
 });
 router.get('/playlist/sad', function(req, res, next) {
-  res.render('playlist/sad', { mood: 'Sad' });
+  res.render('playlist/sad', { mood: 'sad' });
 });
 router.get('/playlist/unknown', function(req, res, next) {
-  res.render('playlist/unknown', { mood: 'Unknown' });
+  res.render('playlist/unknown', { mood: '???' });
 });
 
 module.exports = router;
