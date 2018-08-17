e59f79f4f5e4329fcef38cb607e5f01988568845
fpriemesberger <fpreimesberger@gmail.com>
2018-08-17 14:19:05 -0500

    fixed server and changed keys

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 require('dotenv').config();
+var rp = require('request-promise');
 
 var app = express();
-console.log('aaaaaaaaaaaaaaaaaaaaaaaaa');
-console.log(process.env.APIkey);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
